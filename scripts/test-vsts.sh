5cc18f3fba7b78a1fab903444681aa51524fc6f8
Dan Appello <appellod@gmail.com>
2018-09-03 23:42:01 -0600

    update tests

--- scripts/test-vsts.sh
+++ scripts/test-vsts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+set -e
 
 nyc mocha --exit --reporter mocha-multi-reporters --reporter-options configFile=mocha-multi-reporters.json ./find-users/index.spec.js
-node ./scripts/process-coverage-report.js
